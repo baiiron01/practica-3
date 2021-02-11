5b9e248c03dc28615cc5449ac6ad11ac17892e51
baiiron01 <78938509+baiiron01@users.noreply.github.com>
2021-02-12 07:24:37 +0800

    Jhon Byron Del Rosario 2019-8730
    practica 3

---
+++ practiva 3.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace practica_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+//         •	Crear un programa que pida números positivos al usuario, y vaya calculando
+//la suma de todos ellos(terminará cuando se teclea un número negativo o cero).
+         
+//        int suma = 0;
+//            int ingresado = 0;
+//            while (ingresado >= 0)
+//            {
+//                suma += ingresado;
+//                Console.WriteLine("Ingresar número: ");
+//                ingresado = int.Parse(Console.ReadLine());
+//            }
+
+//            Console.WriteLine($"La suma es {suma}");
+
+ 
+
+
+//•	Crea un programa que escriba en pantalla los números del 1 al 10, usando "do..while".
+//            int contador = 1;
+//            while (contador <= 10)
+//            {
+//                Console.WriteLine(contador);
+//                contador += 1;
+
+//•	Crear un programa que muestre los primeros 10 números pares a partir del producto de(10 x 10).
+
+//            int contador = 101;
+//                while (contador <= 120)
+//                {
+//                    if (contador % 2 == 0)
+//                    {
+//                        Console.WriteLine(contador);
+//                    }
+//                    contador += 1;
+
+
+//•	Crear un programa que muestre las letras de la Z(mayúscula) a la A(mayúscula, descendiendo).
+    
+//            int i;
+//                    Console.Write("descendiendo abecedario");
+//                    Console.WriteLine();
+//                    for (i = 90; i >= 65; i--)
+//                    {
+//                        Console.WriteLine("LETRA: " + Convert.ToString((char)i));
+//                    }
+//                    Console.Write("Pulse una Tecla:"); Console.ReadLine();
+
+                }
+            }
+}
+
